d54708fd80101b6ae4505e99dcda210520c516d7
AndreiNateasin <nateasinandrei@gmail.com>
2014-07-28 17:43:00 +0300

    fixed image displa for consumers

--- GIS/SQL scripts/InsertImages.sql
+++ GIS/SQL scripts/InsertImages.sql
@@ -1,39 +1,39 @@
 ﻿UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\bucuresti.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\bucuresti.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%bucuresti%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\cluj.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\cluj.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%cluj%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\ploiesti.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\ploiesti.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%ploiesti%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\buzau.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\buzau.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%buzau%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\suceava.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\suceava.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%suceava%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\balti.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\balti.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%balti%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\chisinau.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\chisinau.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%chisinau%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\sibiu.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\sibiu.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%sibiu%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\pitesti.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\pitesti.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%pitesti%' 
 
 UPDATE dbo.Consumers 
-SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\Andrei\Pictures\Cities\galati.png', Single_Blob) MyImage)
+SET [Image] = (SELECT MyImage.* from Openrowset(Bulk 'C:\Users\User\Documents\GitHub\GIS\ConsumerImages\galati.png', Single_Blob) MyImage)
 WHERE ConsumerName like '%galati%' 
