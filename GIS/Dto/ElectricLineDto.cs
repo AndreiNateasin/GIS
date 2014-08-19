7e9482254be2d59fe7ccf46043f22d18bf1fc2ab
AndreiNateasin <nateasinandrei@gmail.com>
2014-08-19 09:44:25 +0300

    added missing files

---
+++ GIS/Dto/ElectricLineDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GIS.Dto
+{
+    public class ElectricLineDto
+    {
+        public ElectricLineDto()
+        {
+            Locations = new List<LocationDto>();
+            Image = new byte[] { };
+        }
+
+        public string ConsumerName { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public int Capacity { get; set; }
+
+        public IEnumerable<LocationDto> Locations { get; set; }
+    }
+}
